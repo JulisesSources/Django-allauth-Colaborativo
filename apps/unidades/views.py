09d7e08f548a2258723c10a0f0fe895250653abe
allshxn <alohalo9@hotmail.com>
2025-12-04 01:04:11 -0600

    feat(unidades): implementar módulo completo de Unidades Administrativas - Creación del CRUD completo - Validación para evitar eliminación de unidades con trabajadores asociados - Configuración de admin.py con campos, filtros y auditoría

--- apps/unidades/views.py
+++ apps/unidades/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.db.models.deletion import ProtectedError
+from django.contrib import messages
+from django.urls import reverse_lazy
+from .models import UnidadAdministrativa
 
 
-def index(request):
-    return HttpResponse("App: Unidades Administrativas - Módulo en construcción por Alonso")
+class UnidadListView(ListView):
+    model = UnidadAdministrativa
+    template_name = "unidades/index.html"
+    context_object_name = "unidades"
+
+
+class UnidadCreateView(CreateView):
+    model = UnidadAdministrativa
+    fields = ['nombre', 'descripcion']
+    template_name = "unidades/form.html"
+    success_url = reverse_lazy('unidades:lista')
+
+
+class UnidadUpdateView(UpdateView):
+    model = UnidadAdministrativa
+    fields = ['nombre', 'descripcion']
+    template_name = "unidades/form.html"
+    success_url = reverse_lazy('unidades:lista')
+
+
+class UnidadDeleteView(DeleteView):
+    model = UnidadAdministrativa
+    template_name = "unidades/eliminar.html"
+    success_url = reverse_lazy("unidades:lista")
+
+    def form_valid(self, form):
+        try:
+            return super().form_valid(form)
+        except ProtectedError:
+            messages.error(
+                self.request,
+                "No puedes eliminar esta unidad porque tiene trabajadores relacionados."
+            )
+            return self.render_to_response(self.get_context_data())
