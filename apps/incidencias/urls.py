59208bb5bd1816cc344e6329006f5be2fbafbf07
FernaGa <fernandoart35@outlook.es>
2025-12-05 13:43:45 -0600

    incidencias:actualziar vistas y rutas para manejos de autorización y tipos de incidencias

--- apps/incidencias/urls.py
+++ apps/incidencias/urls.py
@@ -7,12 +7,13 @@ app_name = 'incidencias'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # Lista y gestión de incidencias
     path('lista/', views.lista_incidencias, name='lista_incidencias'),
     path('mis-incidencias/', views.mis_incidencias, name='mis_incidencias'),
+    path('autorizar/', views.autorizar_incidencias, name='autorizar_incidencias'),
     path('crear/', views.crear_incidencia, name='crear_incidencia'),
+    path('tipos/crear/', views.crear_tipo_incidencia, name='crear_tipo_incidencia'),
+    # Rutas basadas en ID
     path('<int:pk>/', views.detalle_incidencia, name='detalle_incidencia'),
-    
     path('<int:pk>/editar/', views.editar_incidencia, name='editar_incidencia'),
     path('<int:pk>/eliminar/', views.eliminar_incidencia, name='eliminar_incidencia'),
     path('<int:pk>/autorizar/', views.autorizar_incidencia, name='autorizar_incidencia'),
