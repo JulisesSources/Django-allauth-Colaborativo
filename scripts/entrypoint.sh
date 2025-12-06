1d6ba6b8363bbd0a41f330fd17a29629023bc3f1
Christian Alfonso <L21290603@cdguzman.tecnm.mx>
2025-12-06 13:47:18 -0600

    fix: validar superusuario por email en lugar de username

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,7 +25,7 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] &&
     python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
     User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
     print('✓ Superusuario creado')
 else:
